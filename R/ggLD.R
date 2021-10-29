fdbbea7a83aa461e8bf64f3a5ce5561bd35ae61f
Minsoo <mmkim1210@gmail.com>
2021-10-29 00:39:53 -0700

    update code

--- R/ggLD.R
+++ R/ggLD.R
@@ -11,43 +11,30 @@
 #' @importFrom rlang .data
 ggLD <- function(data){
   data <- tidyr::as_tibble(data)
+  colnames(data) <- c(1:ncol(data))
   n <- length(data)
 
+  # Tidy data, only taking unique pairs of data
   values <- data %>%
-    dplyr::mutate(idx1 = colnames(data)) %>%
+    dplyr::mutate(idx1 = c(1:nrow(data))) %>%
     tidyr::pivot_longer(!.data$idx1, names_to = "idx2", values_to = "LD") %>%
+    dplyr::mutate(dplyr::across(idx2, as.double)) %>%
     dplyr::filter(!duplicated(paste0(pmax(.data$idx1, .data$idx2), pmin(.data$idx1, .data$idx2)))) %>%
-    tidyr::unite("id", .data$idx1:.data$idx2, remove = FALSE)
+    tidyr::unite("id", .data$idx1:.data$idx2, remove = FALSE) %>%
+    dplyr::mutate(diff = abs(idx2 - idx1))
 
-  positions <- tidyr::tibble(id = rep(values$id, each = 4),
-                             x = 0,
-                             y = 0)
-  positions$x[(nrow(positions) - 3):nrow(positions)] = c(10,
-                                                         10 - 10 / (2 * n),
-                                                         10 - 10 / n,
-                                                         10 - 10 / (2 * n))
-  positions$y[(nrow(positions) - 3):nrow(positions)] = c(5,
-                                                         5 + 10 / (2 * n),
-                                                         5,
-                                                         5 - 10 / (2 * n))
-  fibonacci <- 0
-  for (i in 1:(n - 1)) {
-    fibonacci <- fibonacci + i
-    for (j in 1:(i + 1)) {
-      positions$x[((nrow(positions) - 3) - fibonacci * 4 - (j - 1) * 4):(nrow(positions) - fibonacci * 4 - (j - 1) * 4)] =
-        c(10 - 10 / n * i / 2 - (j - 1) * 10 / n / 2,
-          10 - 10 / (2 * n) - 10 / n * i / 2 - (j - 1) * 10 / n / 2,
-          10 - 10 / n - 10 / n * i / 2 - (j - 1) * 10 / n / 2,
-          10 - 10 / (2 * n) - 10 / n * i / 2 - (j - 1) * 10 / n / 2)
-      positions$y[((nrow(positions) - 3) - fibonacci * 4 - (j - 1) * 4):(nrow(positions) - fibonacci * 4 - (j - 1) * 4)] =
-        c(5 - 10 / n * i / 2 + (j - 1) * 10 / n / 2,
-          5 + 10 / (2 * n) - 10 / n * i / 2 + (j - 1) * 10 / n / 2,
-          5 - 10 / n * i / 2 + (j - 1) * 10 / n / 2,
-          5 - 10 / (2 * n) - 10 / n * i / 2 + (j - 1) * 10 / n / 2)
-    }
-  }
+  # Calculate coordinates for geom_polygon
+  positions <- dplyr::bind_rows(values, values, values, values) %>%
+    dplyr::group_by(diff, idx1) %>%
+    dplyr::mutate(add_index1 = c(0, 1, 0, 1),
+                  add_index2 = c(0, -1, 0, 1),
+                  minus1_index = c(1, 1, 0, 1)) %>%
+    dplyr::mutate(x = diff * 5 / n + 10 / n * (idx1 - minus1_index) + 5 / n * add_index1,
+                  y = 5 - diff * 5 / n + 5 / n * add_index2) %>%
+    dplyr::ungroup()
 
-  merge(values, positions, by = c("id")) %>%
+  # ggplot2
+  positions %>%
     ggplot2::ggplot(ggplot2::aes(x = .data$x, y = .data$y)) +
     ggplot2::geom_polygon(ggplot2::aes(fill = .data$LD, group = .data$id)) +
     ggplot2::theme_void() +
